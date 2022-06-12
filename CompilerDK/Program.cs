e6c21abab463ab27ef4d544f146c6c3d236d45ca
Davi Costa <davi.machado.costa@hotmail.com>
2022-06-11 21:37:40 -0300

    Implementando leitura de arquivo

--- CompilerDK/Program.cs
+++ CompilerDK/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var path = @"E:\davim\GitHub\DKSCompiler\CompilerDK\teste.txt";
+        string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
