256cdb8fdd6af91cdd9d60a3fbe9ffc9125c28f3
Davi Costa <davi.machado.costa@hotmail.com>
2022-06-11 22:02:22 -0300

    Adicionando tratamento de erros na leitura de arquivo

--- CompilerDK/Program.cs
+++ CompilerDK/Program.cs
@@ -1,16 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var path = @"E:\davim\GitHub\DKSCompiler\CompilerDK\teste.txt";
-        string[] lines = File.ReadAllLines(path, Encoding.UTF8);
-        foreach (string line in lines)
+        FileReader();
+    }
+
+  public static void FileReader()
+    {
+        Console.WriteLine(" Enter the path to te .dks format file: ");
+        Console.Write("> ");
+        string path = Console.ReadLine();
+
+        //Exemplo de arquivo para facilitar os testes: E:\davim\GitHub\DKSCompiler\CompilerDK\wirth.dks
+
+        if(string.IsNullOrEmpty(path))
+            Console.WriteLine(" \nERRO: No file specified, please select a .dks file\n");
+        else if (!Path.GetExtension(path).Equals(".dks"))
+            Console.WriteLine(" \nERRO: Invalid file format, please select a .dks extension file\n");
+        else
         {
-            Console.WriteLine(line);
+            try
+            {
+                string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                    foreach (char character in line)
+                        if (character == '.')
+                        {
+
+                        }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("\nERRO: The file cannot be found.\n");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("\nERRO: The directory cannot be found.\n");
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("\nERRO: 'path' exceeds the maxium supported path length.\n");
+            }
+            catch (Exception err) {
+                Console.WriteLine("\nERRO: Ocorreu um erro desconhecido", err);
+            }
         }
     }
 }
