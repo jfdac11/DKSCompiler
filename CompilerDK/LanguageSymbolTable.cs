42b0ece43ae688e1d291dcd6ea46899abbfcf68e
Davi Costa <davi.machado.costa@hotmail.com>
2022-06-23 16:56:53 -0300

    Consertando regex de validação partcial de FloatNumber

--- CompilerDK/LanguageSymbolTable.cs
+++ CompilerDK/LanguageSymbolTable.cs
@@ -27,26 +27,25 @@ namespace CompilerDK
 
         private void CreateAtoms()
         {
-            Atom FloatNumber = new Atom("ID06", @"^[0-9]+\.[0-9]+(e(-|\+)?[0-9]+)?$", @"^[0-9]+\.?[0-9]*(e(-|\+)?[0-9]+)?$");
-            Atoms.Add(FloatNumber);
-            Atom ConstantString = new Atom("ID02", "^\"([a-zA-Z]|\\s|[0-9]|\\$|_|\\.)+\"$", "^\"([a-zA-Z]|\\s|[0-9]|\\$|_|\\.)*\"?$");
-            Atoms.Add(ConstantString);
-            Atom Character = new Atom("ID05", "^'[a-zA-Z]'$", @"^'[a-zA-Z]?'?$");
-            Atoms.Add(Character);
-
             Atom Bool = new Atom("PR01", "^bool$", "^b(o(o(l)?)?)?$");
             Atoms.Add(Bool);
             Atom While = new Atom("PR02", "^while$", "^w(h(i(l(e)?)?)?)?$");
             Atoms.Add(While);
             Atom If = new Atom("PR07", "^if$", "^if?$");
             Atoms.Add(If);
-            // descomentar so depois de fazer a diferenciação por escopo
-            //Atom Identifier = new Atom("ID01", "^(([a-zA-Z]|_)+[0-9]*)+$", "^(([a-zA-Z]|_)+[0-9]*)+$");
-            //Atoms.Add(Identifier);
-            /*Atom Function = new Atom("ID04", "^([a-zA-Z]+[0-9]*)+$", "^([a-zA-Z]+[0-9]*)+$");
-            Atoms.Add(Function);
+
+            Atom Identifier = new Atom("ID01", "^(([a-zA-Z]|_)+[0-9]*)+$", "^(([a-zA-Z]|_)+[0-9]*)+$");
+            Atoms.Add(Identifier);
+            Atom ConstantString = new Atom("ID02", "^\"([a-zA-Z]|\\s|[0-9]|\\$|_|\\.)+\"$", "^\"([a-zA-Z]|\\s|[0-9]|\\$|_|\\.)*\"?$");
+            Atoms.Add(ConstantString);
             Atom IntegerNumber = new Atom("ID03", "^[0-9]+$", "^[0-9]+$");
-            Atoms.Add(IntegerNumber);*/
+            Atoms.Add(IntegerNumber);
+            Atom Function = new Atom("ID04", "^([a-zA-Z]+[0-9]*)+$", "^([a-zA-Z]+[0-9]*)+$");
+            Atoms.Add(Function);
+            Atom Character = new Atom("ID05", "^'[a-zA-Z]'$", @"^'[a-zA-Z]?'?$");
+            Atoms.Add(Character);
+            Atom FloatNumber = new Atom("ID06", @"^[0-9]+\.[0-9]+(e(-|\+)?[0-9]+)?$", @"^[0-9]+(\.([0-9]+((e((-|\+)?[0-9]*)?)?)?)?)?$");
+            Atoms.Add(FloatNumber);
         }
 
     }
