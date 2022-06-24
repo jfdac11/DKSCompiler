2a7a6c69862b5ab04a5cf72e5d0b2d2bf8f3f6a7
fernandanlisboa <fernanda.lisboa@aln.senaicimatec.edu.br>
2022-06-23 21:56:35 -0300

    Create LexicalTableReport.cs

---
+++ CompilerDK/LexicalTableReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerDK
+{
+    internal class LexicalItemTable
+    {
+        public string Lexeme { get; set; }
+        public string AtomCode { get; set; }
+        public int SymbolTableIndex { get; set; }
+
+        public LexicalItemTable(string lexeme, string atomCode, int symbolTableIndex)
+        {
+            Lexeme = lexeme;
+            AtomCode = atomCode;
+            SymbolTableIndex = symbolTableIndex;
+        }
+    }
+
+    internal class LexicalTableReport
+    {
+    }
+}
